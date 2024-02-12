3312c82af69a6da5144f322d2be01fe3973b0440
Deepesh76780 <da76780@gmail.com>
2024-02-13 02:08:47 +0530

    add input box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { socket } from './socket';
-import React, { useState, useEffect } from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { socket } from "./socket";
+import React, { useState, useEffect } from "react";
 
 function App() {
-
   useEffect(() => {
     socket.connect();
     socket.emit("setup", { userId: 1 });
 
-  
     return () => {
       socket.disconnect();
     };
   }, []);
 
-
   return (
     <div className="App">
-      
+      <div className="inputF">
+        <input className="input" placeholder="type here...." />
+      </div>
     </div>
   );
 }
