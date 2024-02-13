4806134b3fc1ec0bbe2b26b2301ca679321779ab
Deepesh76780 <da76780@gmail.com>
2024-02-13 15:48:57 +0530

    change url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL ="http://localhost:5000";
+const URL ="https://worldcup-io-assignment.onrender.com";
 
 export const socket = io(URL, {
   autoConnect: false,
